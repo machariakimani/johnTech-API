29948c6c4e690942b926e1e277759ee9c7aed618
Dev Macharia <kimanijohnmacharia436@gmail.com>
2026-06-16 11:50:11 +0300

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
 	"http://localhost:5173",
 	"http://localhost:5174",
 	"https://john-tech-repairs-t4sv.onrender.com",
+	"https://techrepairs.vercel.app",
 	"https://www.google.com"
 ]
 module.exports = allowedOrigins
